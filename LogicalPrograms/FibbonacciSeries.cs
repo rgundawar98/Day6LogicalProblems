0a78ff24df7ec63f4529920b1275c58b990618b5
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-15 17:18:01 +0530

    Displaying fibbonacci series UC1_FibbonacciSeries

---
+++ LogicalPrograms/FibbonacciSeries.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class FibbonacciSeries
+    {
+        public static void Fibbonacci_Series()
+        {
+            int val1=0,val2=1,val3=2;
+            Console.WriteLine("Enter the range for fibbonacci series");
+            int number = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("The fibbonacci series will be:");
+            Console.WriteLine(val1);
+            Console.WriteLine(val2);
+            for(val3=2;val3<=number;val3++)
+            {
+                val3= val1 + val2;
+                Console.WriteLine(val3);
+                val1 = val2;
+                val2= val3;
+            }
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -7,6 +7,15 @@ namespace LogicalPrograms
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to the Logical Programs");
+            Console.WriteLine("1.Fibbonacci Series");
+            Console.WriteLine("Choose option from above");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch(option)
+            {
+                case 1:
+                    FibbonacciSeries.Fibbonacci_Series();
+                    break;
+            }
         }
     }
 }
