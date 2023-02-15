90fb6e6f46cc598490b02756aacd275f1c376b35
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-15 17:09:45 +0530

    Adding welcome message

---
+++ LogicalPrograms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LogicalPrograms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Logical Programs");
+        }
+    }
+}
