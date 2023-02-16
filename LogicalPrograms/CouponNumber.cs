4cc63e779bf2f421a216f478dd5f793e2cdfbe6d
rahulgundawar98 <rahulgundawar98@gmail.com>
2023-02-16 20:42:27 +0530

    Calculate how many random numbers to found distinct coupon number UC5_CouponNumber

---
+++ LogicalPrograms/CouponNumber.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicalPrograms
+{
+    public class CouponNumber
+    {
+        public static void Check_Coupon_Number(int CouponNumber)
+        {
+            Console.WriteLine("Enter the distinct coupon number");
+            CouponNumber = Convert.ToInt32(Console.ReadLine());
+            int distinct=0 ,count=0;
+            bool[] isCollected = new bool[CouponNumber];
+            int[] Coupons = new int[CouponNumber];
+            while(distinct < CouponNumber)
+            {
+                Random random= new Random();
+                int newRandom = Convert.ToInt32(random.Next(0,CouponNumber));
+                count++;
+                if (!isCollected[newRandom])
+                {
+                    distinct++;
+                    isCollected[newRandom] = true;
+                    Coupons[newRandom] = newRandom;
+                    Console.WriteLine(Coupons[newRandom]);
+                }
+            }
+            Console.WriteLine("Total random number needed to have all distinct count :"+count);
+        }
+    }
+}
--- LogicalPrograms/Program.cs
+++ LogicalPrograms/Program.cs
@@ -9,7 +9,7 @@ namespace LogicalPrograms
         {
             Console.WriteLine("Welcome to the Logical Programs");
             Console.WriteLine("1.Fibbonacci Series\n2.Perfect Number\n3.PrimeNumber\n4.ReverseNumber\n6.Calulate StopWatch" +
-                "\n8.DayOfWeek\n9.Temperature Conversion\n10.MonthlyPayment\n11.SquraeRoot\n7.Check Amount");
+                "\n8.DayOfWeek\n9.Temperature Conversion\n10.MonthlyPayment\n11.SquraeRoot\n7.Check Amount\n5.Coupon Number");
             Console.WriteLine("Choose option from above");
             int option = Convert.ToInt32(Console.ReadLine());
             switch(option)
@@ -43,6 +43,9 @@ namespace LogicalPrograms
                     break;
                 case 7:
                     VendingMachine.Check_Amount();
+                    break;
+                case 5:
+                    CouponNumber.Check_Coupon_Number(50);
                     break;
             }
         }
